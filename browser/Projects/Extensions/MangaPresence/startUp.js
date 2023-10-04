8e48074b82cafd6f5b479febd1c42d733a6820de
Sandelier <miika.saily@gmail.com>
2023-10-04 23:24:45 +0300

    Remade "MangaPresence" starting window.

--- browser/Projects/Extensions/MangaPresence/startUp.js
+++ browser/Projects/Extensions/MangaPresence/startUp.js
@@ -1,33 +1,23 @@
-const bookImage = document.getElementById('book-image');
+const bookVideo = document.getElementById('book-video');
 const animationContainer = document.getElementById('book-anim-container');
 const startupContainer = document.getElementById('startUp-container');
+const videoMap = document.getElementById('video-click-map');
 
-bookImage.addEventListener('mouseenter', function() {
-    // settimeout 1milliseconds for firefox rendering. 
-    setTimeout(function() {
-        bookImage.src = 'spinningBook.gif';
-
-        // Gif is little bit smaller then the picture so we have to increase the height.
-        startupContainer.style.height = '94vh';
-    }, 1);
-});
-
-let isTransition = false; // Used so if user clicks the gif and then mvoes mouse he wont see it returning back to test.webp
-
-bookImage.addEventListener('mouseout', function() {
-    if (!isTransition) {
-        bookImage.src = 'book.webp';
-        startupContainer.style.height = '80vh';
-    }        
-});
-
-bookImage.addEventListener('click', function() {
-    startupContainer.style.filter = 'blur(5px)';
-    isTransition = true;
+bookVideo.addEventListener('ended', function () {
+    startupContainer.style.filter = 'none';
     animationContainer.style.transform = 'translateY(-100%)';
-    
-    setTimeout(function() {
+
+    setTimeout(function () {
         startupContainer.remove();
         adjustFontSizeToFitPages();
     }, 500);
 });
+
+videoMap.addEventListener('click', function () {
+    if (bookVideo.paused) {
+        videoMap.style.display = 'none';
+        bookVideo.play();
+    }
+});
+
+bookVideo.pause();
